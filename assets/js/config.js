1da081d72c0c77514896cc1cf3157d15b56a6886
POOL <pool@mac.mynetworksettings.com>
2025-10-01 11:56:54 -0400

    fix(api): Update affiliate API token

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@
 const CONFIG = {
     USE_MOCK_DATA: false, // Cambia a 'false' cuando tengas un token de API válido
     API_BASE_URL: 'https://affiliate.tutellus.com/api/v1',
-    AFFILIATE_API_TOKEN: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJadDJIZHA4UHAiLCJhdWQiOiJhZmZpbGlhdGUiLCJhZmZfdG9rZW4iOiI2OWZkM2YwNjVlZjQ0Y2VmODY2ZjNhMmQyZjc1NmIzMyIsImlhdCI6MTc1ODMyMDA1N30.7dDOrpcDCnsnTJ2yPCwW9cvdTCsoQS_QNJhW_9gKV_U',
+    AFFILIATE_API_TOKEN: 'NUEVO_TOKEN_AQUI', // ¡IMPORTANTE! Reemplaza esto con tu nuevo token de la API de Tutellus.
     AFFILIATE_ID: '69fd3f065ef44cef866f3a2d2f756b33',
     STORAGE_KEYS: {
         USER: 'user_data'

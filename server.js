832cf449a9431822a3ec8914e284a43e69f08a3c
POOL <pool@mac.mynetworksettings.com>
2025-10-06 16:51:10 -0400

    fix(server): aÃ±ade soporte para pathPrefix y sirve desde _site

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const fs = require('fs');
 const path = require('path');
 
 const port = 3000;
+const outputDir = '_site';
+const pathPrefix = '/website';
 
 const server = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
-    
+    // Remove the path prefix from the request URL
+    let url = req.url;
+    if (url.startsWith(pathPrefix)) {
+        url = url.substring(pathPrefix.length);
+    }
+
+    let filePath = path.join(__dirname, outputDir, url === '/' ? 'index.html' : url);
+
     const extname = path.extname(filePath);
     let contentType = 'text/html';
-    
+
     switch(extname) {
         case '.css':
             contentType = 'text/css';
@@ -24,11 +32,24 @@ const server = http.createServer((req, res) => {
             contentType = 'image/jpg';
             break;
     }
-    
+
     fs.readFile(filePath, (err, content) => {
         if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.end('<h1>404 - Página no encontrada</h1>');
+            // If file not found, try to append .html
+            if (err.code === 'ENOENT' && !extname) {
+                fs.readFile(filePath + '.html', (err2, content2) => {
+                    if (err2) {
+                        res.writeHead(404, { 'Content-Type': 'text/html' });
+                        res.end('<h1>404 - Página no encontrada</h1>');
+                    } else {
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        res.end(content2);
+                    }
+                });
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>404 - Página no encontrada</h1>');
+            }
         } else {
             res.writeHead(200, { 'Content-Type': contentType });
             res.end(content);
@@ -38,4 +59,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
+    console.log(`Abre tu navegador en http://localhost:${port}/website/blog.html`);
 });
